f3b1f6698db3addb92ffbdcb84b5762b74822346
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2024-06-21 23:36:55 +0200

    redo structure

---
+++ frontend/app/components/side_nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface SideNavProps {
+    // Define any props you need for your side nav component
+}
+
+const SideNav: React.FC<SideNavProps> = (props) => {
+    // Implement your side nav component logic here
+
+    return (
+        <div>
+            {/* Add your side nav content here */}
+        </div>
+    );
+};
+
+export default SideNav;
