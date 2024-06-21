bcfcb5c0e8d314918a2f2e36c682dc184c21bc7a
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2024-06-21 23:59:06 +0200

    init page.tsx

--- frontend/app/components/side_nav.tsx
+++ frontend/app/components/side_nav.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
-interface SideNavProps {
-    // Define any props you need for your side nav component
-}
-
-const SideNav: React.FC<SideNavProps> = (props) => {
-    // Implement your side nav component logic here
+const SideNav = () => {
 
     return (
-        <div>
-            {/* Add your side nav content here */}
-        </div>
+        <aside>
+            <ul className="grid h-[92vh] bg-[var(--secondary)] w-[20vw] justify-start ">
+                <li><a href="/#">Dashboard</a></li>
+                <li><a href="/#">Befunde</a></li>
+                <li><a href="/#">Krankheitsbild</a></li>
+                <li><a href="/#">Medikament</a></li>
+                <li><a href="/#">Impfungen</a></li>
+            </ul>
+        </aside>
     );
 };
 
