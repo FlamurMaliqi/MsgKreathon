205e5699634b69cdd9e4f7bdbfa8ee12c67b9a1d
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2024-06-23 02:07:07 +0200

    pushinng uff

--- frontend/app/Krankheitsbild/EditAllergie/page.tsx
+++ frontend/app/Krankheitsbild/EditAllergie/page.tsx
@@ -47,96 +47,91 @@ export default function Home() {
 
 
     return (
-        <main className="main-grid grid min-h-screen">
-            <HeaderNav></HeaderNav>
-            <SideNav />
-            <div className="content h-[92vh] w-[88vw] overflow-y-scroll py-4 grid grid-cols-1 content-evenly justify-items-center">
-                <form className="w-full max-w-lg p-4 bg-[--secondary] rounded">
-                    <h3>Impfungsinformationen </h3>
-                    
-                    <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="content h-[92vh] w-[88vw] overflow-y-scroll py-4 grid grid-cols-1 content-evenly justify-items-center">
+            <form className="w-full max-w-lg p-4 bg-[--secondary] rounded">
+                <h3>Impfungsinformationen </h3>
+                
+                <div className="flex flex-wrap -mx-3 mb-6">
 
-                        <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-[var(--onTritary)] text-xs font-bold mb-2" htmlFor="grid-password">
-                                Reaktion
-                            </label>
-                            <Input className="appearance-none block w-full bg-gray-100 text-[var(--onTritary)] border border-[var(--onTritary)] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[var(--onTritary)]-500"
-                                json={d}
-                                jsonPath="reaction"
-                                displayName="Reaktion"
-                                updateFunction={() => {
-                                }}
-                            />
-                        </div>
-
-                        <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-[var(--onTritary)] text-xs font-bold mb-2" htmlFor="grid-password">
-                                Schweregrad
-                            </label>
-                            <Input className="appearance-none block w-full bg-gray-100 text-[var(--onTritary)] border border-[var(--onTritary)] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[var(--onTritary)]-500"
-                                json={d}
-                                jsonPath="severity"
-                                displayName="Schweregrad"
-                                updateFunction={() => {
-                                }}
-                            />
-                        </div>
-
-                        <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-[var(--onTritary)] text-xs font-bold mb-2" htmlFor="grid-password">
-                                Notizen
-                            </label>
-                            <Input className="appearance-none block w-full bg-gray-100 text-[var(--onTritary)] border border-[var(--onTritary)] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[var(--onTritary)]-500"
-                                json={d}
-                                jsonPath="notes"
-                                displayName="Notizen"
-                                updateFunction={() => {
-                                }}
-                            />
-                        </div>
-
-                        <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-[var(--onTritary)] text-xs font-bold mb-2" htmlFor="grid-password">
-                                Diagnosedatum
-                            </label>
-                            <JsonConnectedDatePicker
-                                className="appearance-none block w-full bg-gray-100 text-[var(--onTritary)] border border-[var(--onTritary)] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[var(--onTritary)]-500"
-                                json={d}
-                                jsonPath="dateDiagnosed"
-                                displayName="Diagnosedatum"
-                                updateFunction={() => {
-                                }}
-                            />
-                        </div>
-
+                    <div className="w-full px-3">
+                        <label className="block uppercase tracking-wide text-[var(--onTritary)] text-xs font-bold mb-2" htmlFor="grid-password">
+                            Reaktion
+                        </label>
+                        <Input className="appearance-none block w-full bg-gray-100 text-[var(--onTritary)] border border-[var(--onTritary)] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[var(--onTritary)]-500"
+                            json={d}
+                            jsonPath="reaction"
+                            displayName="Reaktion"
+                            updateFunction={() => {
+                            }}
+                        />
                     </div>
-                   
-                   
+
+                    <div className="w-full px-3">
+                        <label className="block uppercase tracking-wide text-[var(--onTritary)] text-xs font-bold mb-2" htmlFor="grid-password">
+                            Schweregrad
+                        </label>
+                        <Input className="appearance-none block w-full bg-gray-100 text-[var(--onTritary)] border border-[var(--onTritary)] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[var(--onTritary)]-500"
+                            json={d}
+                            jsonPath="severity"
+                            displayName="Schweregrad"
+                            updateFunction={() => {
+                            }}
+                        />
+                    </div>
+
+                    <div className="w-full px-3">
+                        <label className="block uppercase tracking-wide text-[var(--onTritary)] text-xs font-bold mb-2" htmlFor="grid-password">
+                            Notizen
+                        </label>
+                        <Input className="appearance-none block w-full bg-gray-100 text-[var(--onTritary)] border border-[var(--onTritary)] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[var(--onTritary)]-500"
+                            json={d}
+                            jsonPath="notes"
+                            displayName="Notizen"
+                            updateFunction={() => {
+                            }}
+                        />
+                    </div>
+
+                    <div className="w-full px-3">
+                        <label className="block uppercase tracking-wide text-[var(--onTritary)] text-xs font-bold mb-2" htmlFor="grid-password">
+                            Diagnosedatum
+                        </label>
+                        <JsonConnectedDatePicker
+                            className="appearance-none block w-full bg-gray-100 text-[var(--onTritary)] border border-[var(--onTritary)] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[var(--onTritary)]-500"
+                            json={d}
+                            jsonPath="dateDiagnosed"
+                            displayName="Diagnosedatum"
+                            updateFunction={() => {
+                            }}
+                        />
+                    </div>
+
+                </div>
+                
+                
 
 
-                </form>
+            </form>
 
-                <button className="bg-transparent hover:bg-[var(--primary)] text-[var(--primary)] font-semibold hover:text-[var(--onPrimary)] py-2 px-4 border border-[var(--primary)] hover:border-transparent rounded" 
-                    type="button"
-                    onClick={async () => {
-                        // get all allergies and update the one with the id
-                        const allergies = await getAllergies(patientId);
-                        for (let i = 0; i < allergies.length; i++) {
-                            if (allergies[i].id == allergyId) {
-                                allergies[i] = d;
-                                break;
-                            }
+            <button className="bg-transparent hover:bg-[var(--primary)] text-[var(--primary)] font-semibold hover:text-[var(--onPrimary)] py-2 px-4 border border-[var(--primary)] hover:border-transparent rounded" 
+                type="button"
+                onClick={async () => {
+                    // get all allergies and update the one with the id
+                    const allergies = await getAllergies(patientId);
+                    for (let i = 0; i < allergies.length; i++) {
+                        if (allergies[i].id == allergyId) {
+                            allergies[i] = d;
+                            break;
                         }
-                        await updateAllergies(patientId, allergies);
-                        alert("Allergie wurde gespeichert")
-                        window.location.href = "/Krankheitsbild?" + "patientId=" + patientId + "&allergyId=" + allergyId;
-                    }}
-                    >
-                    Speichern
-                </button>
+                    }
+                    await updateAllergies(patientId, allergies);
+                    alert("Allergie wurde gespeichert")
+                    window.location.href = "/Krankheitsbild?" + "patientId=" + patientId + "&allergyId=" + allergyId;
+                }}
+                >
+                Speichern
+            </button>
 
-            </div>
-         
-        </main>
+        </div>
     );
 }
--- frontend/app/components/Krankheitsbild.tsx
+++ frontend/app/components/Krankheitsbild.tsx
@@ -8,16 +8,15 @@ export default function Krankheitsbild({diagnoses}:{diagnoses: Diagnosis[]}) {
     const [dialogData, setData] = useState(diagnoses);
     const [dialogContent, setDialogContent] = useState('' as string);
 
-    const handleCardClick = (e: any) => {
-        console.log(e);
+    const handleCardClick = () => {
+        console.log(dialogData);
         setOpen(true);
-        setData(e.node.data);
     };
 
     return (
         <div className="grid grid-cols-3 gap-4 pt-4 w-max">
             {diagnoses.map((diagnosis, index) => (
-                <div key={index} className='col-span-1 rounded-lg bg-gray-200' onClick={handleCardClick}>
+                <div key={index} className="col-span-1 rounded-lg bg-gray-200" onClick={handleCardClick}>
                     <Card 
                         title={diagnosis.illness} 
                         text={
